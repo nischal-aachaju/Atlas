df0664a4df9b29a0cf06cedb01eb2c5b3fd5973f
nyniraula <nyniraula@gmail.com>
2026-01-15 14:58:52 +0000

    ex

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import UIButtons from './components/UIButtons'
+import UIButtons from './components/UIButtonsex'
 
 const App = () => {
   return (
     <div className='bg-black  w-full h-screen text-white'>
       hello
-      <UIButtons />
+      <UIButtonsex />
     </div>
   )
 }
