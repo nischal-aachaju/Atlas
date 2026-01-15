6c3ba5ca33b276e069b8c9eecba89e56ea176825
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-15 20:25:54 +0545

    btn

--- src/components/UIButtons.jsx
+++ src/components/UIButtons.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const UIButtons = () => {
   return (
     <div>
-      <button className=' bg-blue-500 rounded-2xl px-3 py-1 text border-2 border-amber-300'> click me</button>
+      <button className=' bg-blue-500 rounded-2xl px-3 py-1 text border-2 border-amber-300'> click me (Nischal)</button>
     </div>
   )
 }
