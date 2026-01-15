4fbbc4b6a5cf00a46d71951cc066dd6ba1d16dde
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-15 20:44:12 +0545

    UI component deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import UIButtons from './components/UIButtons'
+
 
 const App = () => {
   return (
     <div className='bg-black  w-full h-screen text-white'>
       hello
-      <UIButtons />
     </div>
   )
 }
