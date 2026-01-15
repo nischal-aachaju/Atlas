7fee2e57d687303fd9d7cbddcdbc3f6dabb972c2
nyniraula <nyniraula@gmail.com>
2026-01-15 15:14:15 +0000

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import UIButtons from './components/UIButtons'
 
 const App = () => {
   return (
     <div className='bg-black  w-full h-screen text-white'>
       hello
-      <UIButtons />
     </div>
   )
 }
